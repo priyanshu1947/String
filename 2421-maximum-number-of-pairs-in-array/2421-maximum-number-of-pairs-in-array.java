38bc6375c878a43aa7a412802d3da259f540889f
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-25 22:01:25 +0530

    Time: 0 ms (100%), Space: 40.5 MB (99%) - LeetHub

---
+++ 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] numberOfPairs(int[] nums) 
+    {
+        int pairs=0, leftover =0;
+        int arr [] = new int [101];
+        for (int i = 0; i < nums.length; i++) {
+        int num = nums[i];
+        arr[num]++;
+    }
+        
+        for (int i = 0; i < arr.length; i++) 
+        {
+            
+            int num = arr[i];
+            pairs += num / 2;
+            leftover += num % 2;
+        }
+        return new int [] {pairs,leftover};
+    }
+    
+}
