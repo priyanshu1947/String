843fe83f71af9fb2bca07d5ebba51c1bcb22db2d
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-26 23:03:43 +0530

    Time: 1 ms (100%), Space: 41.5 MB (44.81%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,42 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0;
-        Boolean isPos = true;
+        int n = s.length();
+        int i=0;
+        int result=0;
+        int sign =1;
 
-        while(i < s.length() && s.charAt(i) == ' ')
-            i++;
-
-        if(i == s.length())
-            return 0;
-
-        if(s.charAt(i) == '-'){
-            isPos = false;
+        while(i<n&&s.charAt(i)==' ')
+        i++;
+        if(i<n&&s.charAt(i)=='+')
+        {
+            sign = 1;
             i++;
         }
-        else if(s.charAt(i) == '+')
+        else if (i<n&&s.charAt(i)=='-')
+        {
+            sign=-1;
             i++;
-
-        long ans = 0;
-
-        for(; i < s.length(); i++){
-            if(s.charAt(i) < '0' || s.charAt(i) > '9')
-                break;
-
-            ans = ans*10 + s.charAt(i)-'0';
-
-            if(isPos && ans > (Math.pow(2, 31)-1)){
-                ans = (long)(Math.pow(2, 31)-1);
-                break;
-            }
-            else if(!isPos && ans > Math.pow(2, 31)){
-                ans = (long)Math.pow(2, 31);
-                break;
-            }
         }
-
-        if(!isPos)
-            return (int)(-1*ans);
-
-        return (int)ans;
+         while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+        if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE/10 && digit > 7))
+        {
+            return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        }
+            result = result*10+digit;
+            i++;
+        }
+        return result*sign;
     }
 }
